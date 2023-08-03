3b0784c9505a2d9bf3cc6dd5eb6787dbe1a480e5
Michaela Brown <michaelarbrown4@gmail.com>
2023-08-03 11:03:10 -0500

    Added CSS loaders and babel to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Just Another Text Editor'
@@ -51,7 +51,21 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
